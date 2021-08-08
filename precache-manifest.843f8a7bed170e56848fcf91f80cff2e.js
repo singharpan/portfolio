ae95db3d4c2cab2f4dd3de7c79d84970c8f8fbef
singharpan <singharpan@gmail.com>
2021-08-08 22:14:52 +0530

    Updates

---
+++ precache-manifest.843f8a7bed170e56848fcf91f80cff2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddb6552471c217149e6252a100ee301",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63a4c79bc36118d9e9ea",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f3677de57abf632b9421",
+    "url": "/portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "63a4c79bc36118d9e9ea",
+    "url": "/portfolio/static/js/2.a8d2d013.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.a8d2d013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3677de57abf632b9421",
+    "url": "/portfolio/static/js/main.5282b5d4.chunk.js"
+  },
+  {
+    "revision": "cd61ab05a1da2e66bea8",
+    "url": "/portfolio/static/js/runtime-main.13496ddb.js"
+  }
+]);
